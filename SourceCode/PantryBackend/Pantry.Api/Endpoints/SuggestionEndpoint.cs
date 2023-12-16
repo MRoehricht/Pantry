456918a5500bcae26518ba4aee844cd0e7583c44
Matthias Röhricht <58531609+MRoehricht@users.noreply.github.com>
2023-12-16 10:57:00 +0100

    Update SuggestionEndpoint.cs

--- SourceCode/PantryBackend/Pantry.Api/Endpoints/SuggestionEndpoint.cs
+++ SourceCode/PantryBackend/Pantry.Api/Endpoints/SuggestionEndpoint.cs
@@ -4,10 +4,11 @@ using Pantry.Api.Database.Contexts;
 using Pantry.Shared.Models.GoodModels;
 
 namespace Pantry.Api.Endpoints {
-    public static class SuggestionEndpoint {
-        public static RouteGroupBuilder MapSuggestionEndpoints(this RouteGroupBuilder group) {
+    public static class SuggestionEndpoint 
+    {
+        public static RouteGroupBuilder MapSuggestionEndpoints(this RouteGroupBuilder group) 
+        {
             group.MapGet("/", GetSuggestions).WithName("GetSuggestions").Produces<GoodSuggestion>().WithTags("GoodSuggestion").WithOpenApi();
-
             return group;
         }
 
