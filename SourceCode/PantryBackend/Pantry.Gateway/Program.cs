d182d2776f58f344087aa694515687e20cc8999b
Matthias Röhricht <58531609+MRoehricht@users.noreply.github.com>
2025-01-05 14:10:04 +0100

    Adding simple AuthorizationPolicy

--- SourceCode/PantryBackend/Pantry.Gateway/Program.cs
+++ SourceCode/PantryBackend/Pantry.Gateway/Program.cs
@@ -1,10 +1,27 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services
+    .AddAuthentication(BearerTokenDefaults.AuthenticationScheme)
+    .AddBearerToken();
+
 var app = builder.Build();
 
+app.MapGet("login", () => Results.SignIn(
+    new ClaimsPrincipal(
+        new ClaimsIdentity(
+            [
+                new Claim("sub", Guid.NewGuid().ToString())
+            ], BearerTokenDefaults.AuthenticationScheme)),
+    authenticationScheme: BearerTokenDefaults.AuthenticationScheme));
+
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapReverseProxy();
 app.Run();
 
