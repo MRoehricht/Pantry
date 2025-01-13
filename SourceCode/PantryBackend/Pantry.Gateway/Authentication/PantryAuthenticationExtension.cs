76f08c0a47d01163fefd9697aa832b46000f5d3d
Matthias Röhricht <58531609+MRoehricht@users.noreply.github.com>
2025-01-13 21:00:21 +0100

    Github Auth

---
+++ SourceCode/PantryBackend/Pantry.Gateway/Authentication/PantryAuthenticationExtension.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pantry.Gateway.Authentication;
+
+public static class PantryAuthenticationExtension
+{
+    public static IServiceCollection AddPantryAuthentication(this IServiceCollection services)
+    {
+        services.AddDbContext<DbContext>(options =>
+        {
+            options.UseInMemoryDatabase("db");
+            options.UseOpenIddict();
+        });
+
+
+        services.AddOpenIddict()
+
+            // Register the OpenIddict core components.
+            .AddCore(options =>
+            {
+                // Configure OpenIddict to use the Entity Framework Core stores and models.
+                options.UseEntityFrameworkCore()
+                    .UseDbContext<DbContext>();
+            })
+
+            // Register the OpenIddict client components.
+            .AddClient(options =>
+            {
+                // Allow the OpenIddict client to negotiate the authorization code flow.
+                options.AllowAuthorizationCodeFlow();
+
+                // Register the signing and encryption credentials used to protect
+                // sensitive data like the state tokens produced by OpenIddict.
+                options.AddDevelopmentEncryptionCertificate()
+                    .AddDevelopmentSigningCertificate();
+
+                // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+                options.UseAspNetCore()
+                    .EnableRedirectionEndpointPassthrough();
+
+                // Register the GitHub integration.
+                options.UseWebProviders()
+                    .AddGitHub(options =>
+                    {
+                        options.SetClientId("Ov23lidCCKma1TXEXzee")
+                            .SetClientSecret("7fb2f1169ccba051fa51426d3dcb8672bec1e64c")
+                            .SetRedirectUri("callback/login/github");
+                    });
+            })
+            // Register the OpenIddict validation components.
+            .AddValidation(options =>
+            {
+                // Import the configuration from the local OpenIddict server instance.
+                options.UseLocalServer();
+
+                // Register the ASP.NET Core host.
+                options.UseAspNetCore();
+            });
+        
+        
+
+        return services;
+    }
+
+
+    public static IServiceCollection AddPantryAuthenticationFull(this IServiceCollection services)
+    {
+        services.AddDbContext<DbContext>(options =>
+        {
+            options.UseSqlite($"Filename={Path.Combine(Path.GetTempPath(), "openiddict-mimban-server.sqlite3")}");
+            options.UseOpenIddict();
+        });
+
+        services.AddOpenIddict()
+
+            // Register the OpenIddict core components.
+            .AddCore(options =>
+            {
+                options.UseEntityFrameworkCore()
+                    .UseDbContext<DbContext>();
+            })
+
+            // Register the OpenIddict client components.
+            .AddClient(options =>
+            {
+                // Note: this sample uses the code flow, but you can enable the other flows if necessary.
+                options.AllowAuthorizationCodeFlow();
+
+                // Register the signing and encryption credentials used to protect
+                // sensitive data like the state tokens produced by OpenIddict.
+                options.AddDevelopmentEncryptionCertificate()
+                    .AddDevelopmentSigningCertificate();
+
+                // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+                options.UseAspNetCore()
+                    .EnableRedirectionEndpointPassthrough();
+
+                // Register the System.Net.Http integration and use the identity of the current
+                // assembly as a more specific user agent, which can be useful when dealing with
+                // providers that use the user agent as a way to throttle requests (e.g Reddit).
+                options.UseSystemNetHttp()
+                    .SetProductInformation(typeof(Program).Assembly);
+
+                // Register the Web providers integrations.
+                //
+                // Note: to mitigate mix-up attacks, it's recommended to use a unique redirection endpoint
+                // URI per provider, unless all the registered providers support returning a special "iss"
+                // parameter containing their URL as part of authorization responses. For more information,
+                // see https://datatracker.ietf.org/doc/html/draft-ietf-oauth-security-topics#section-4.4.
+                options.UseWebProviders()
+                    .AddGitHub(options =>
+                    {
+                        options.SetClientId("Ov23lidCCKma1TXEXzee")
+                            .SetClientSecret("7fb2f1169ccba051fa51426d3dcb8672bec1e64c")
+                            .SetRedirectUri("callback/login/github");
+                    });
+            })
+
+            // Register the OpenIddict server components.
+            .AddServer(options =>
+            {
+                // Enable the authorization and token endpoints.
+                options.SetAuthorizationEndpointUris("authorize")
+                    .SetTokenEndpointUris("token");
+
+                // Note: this sample only uses the authorization code flow but you can enable
+                // the other flows if you need to support implicit, password or client credentials.
+                options.AllowAuthorizationCodeFlow();
+
+                // Register the signing and encryption credentials.
+                options.AddDevelopmentEncryptionCertificate()
+                    .AddDevelopmentSigningCertificate();
+
+                // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+                //
+                // Note: unlike other samples, this sample doesn't use token endpoint pass-through
+                // to handle token requests in a custom MVC action. As such, the token requests
+                // will be automatically handled by OpenIddict, that will reuse the identity
+                // resolved from the authorization code to produce access and identity tokens.
+                //
+                options.UseAspNetCore()
+                    .EnableAuthorizationEndpointPassthrough();
+            })
+
+            // Register the OpenIddict validation components.
+            .AddValidation(options =>
+            {
+                // Import the configuration from the local OpenIddict server instance.
+                options.UseLocalServer();
+
+                // Register the ASP.NET Core host.
+                options.UseAspNetCore();
+            });
+
+        services.AddAuthorization()
+            .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        
+            .AddCookie("OpenIddict.Server.AspNetCore");
+
+
+        return services;
+    }
+}
--- SourceCode/PantryBackend/Pantry.Gateway/Program.cs
+++ SourceCode/PantryBackend/Pantry.Gateway/Program.cs
@@ -1,27 +1,140 @@
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using OpenIddict.Abstractions;
+using OpenIddict.Client.WebIntegration;
+using OpenIddict.Server.AspNetCore;
+using OpenIddict.Validation.AspNetCore;
+using Pantry.Gateway.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
-builder.Services
-    .AddAuthentication(BearerTokenDefaults.AuthenticationScheme)
-    .AddBearerToken();
+
+
+
+
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie();
+
+builder.Services.AddAuthorization();
+
+
+
+builder.Services.AddDbContext<DbContext>(options =>
+{
+    options.UseInMemoryDatabase("db");
+    options.UseOpenIddict();
+});
+
+builder.Services.AddOpenIddict()
+
+    // Register the OpenIddict core components.
+    .AddCore(options =>
+    {
+        // Configure OpenIddict to use the Entity Framework Core stores and models.
+        options.UseEntityFrameworkCore()
+            .UseDbContext<DbContext>();
+    });
+
+builder.Services.AddOpenIddict()
+    // Register the OpenIddict client components.
+    .AddClient(options =>
+    {
+        // Allow the OpenIddict client to negotiate the authorization code flow.
+        options.AllowAuthorizationCodeFlow();
+
+        // Register the signing and encryption credentials used to protect
+        // sensitive data like the state tokens produced by OpenIddict.
+        options.AddDevelopmentEncryptionCertificate()
+            .AddDevelopmentSigningCertificate();
+
+        // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+        options.UseAspNetCore()
+            .EnableRedirectionEndpointPassthrough();
+
+        // Register the GitHub integration.
+        options.UseWebProviders()
+            .AddGitHub(options =>
+            {
+                options.SetClientId("[]")
+                    .SetClientSecret("][")
+                    .SetRedirectUri("callback/login/github");
+            });
+    });
 
 var app = builder.Build();
 
-app.MapGet("login", () => Results.SignIn(
-    new ClaimsPrincipal(
-        new ClaimsIdentity(
-            [
-                new Claim("sub", Guid.NewGuid().ToString())
-            ], BearerTokenDefaults.AuthenticationScheme)),
-    authenticationScheme: BearerTokenDefaults.AuthenticationScheme));
-
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
+
+// Add a minimal action responsible for triggering a GitHub challenge
+// and redirecting the user agent to the GitHub authorization endpoint.
+app.MapGet("challenge",
+    () => Results.Challenge(properties: null,
+        authenticationSchemes: [OpenIddictClientWebIntegrationConstants.Providers.GitHub]));
+
+app.MapMethods("callback/login/github", [HttpMethods.Get, HttpMethods.Post], async (HttpContext context) =>
+{
+    // Retrieve the authorization data validated by OpenIddict as part of the callback handling.
+    var result = await context.AuthenticateAsync(OpenIddictClientWebIntegrationConstants.Providers.GitHub);
+
+    // Build an identity based on the external claims and that will be used to create the authentication cookie.
+    var identity = new ClaimsIdentity(authenticationType: "ExternalLogin");
+
+    // By default, OpenIddict will automatically try to map the email/name and name identifier claims from
+    // their standard OpenID Connect or provider-specific equivalent, if available. If needed, additional
+    // claims can be resolved from the external identity and copied to the final authentication cookie.
+    identity.SetClaim(ClaimTypes.Email, result.Principal!.GetClaim(ClaimTypes.Email))
+            .SetClaim(ClaimTypes.Name, result.Principal!.GetClaim(ClaimTypes.Name))
+            .SetClaim(ClaimTypes.NameIdentifier, result.Principal!.GetClaim(ClaimTypes.NameIdentifier));
+
+    // Preserve the registration details to be able to resolve them later.
+    identity.SetClaim(OpenIddictConstants.Claims.Private.RegistrationId, result.Principal!.GetClaim(OpenIddictConstants.Claims.Private.RegistrationId))
+            .SetClaim(OpenIddictConstants.Claims.Private.ProviderName, result.Principal!.GetClaim(OpenIddictConstants.Claims.Private.ProviderName));
+
+    // Build the authentication properties based on the properties that were added when the challenge was triggered.
+    var properties = new AuthenticationProperties(result.Properties.Items)
+    {
+        RedirectUri = "/whoami" //result.Properties.RedirectUri ?? "/whoami"
+    };
+
+    // Ask the default sign-in handler to return a new cookie and redirect the
+    // user agent to the return URL stored in the authentication properties.
+    //
+    // For scenarios where the default sign-in handler configured in the ASP.NET Core
+    // authentication options shouldn't be used, a specific scheme can be specified here.
+    return Results.SignIn(new ClaimsPrincipal(identity), properties);
+});
+
+app.MapGet("whoami", async (HttpContext context) =>
+{
+    var result = await context.AuthenticateAsync();
+    if (result is not { Succeeded: true })
+    {
+        return Results.Text("You're not logged in.");
+    }
+
+    return Results.Text(string.Format("You are {0}.", result.Principal.FindFirst(ClaimTypes.Name)!.Value));
+});
+
+app.MapGet("signout", async (HttpContext context) =>
+{
+    await context.SignOutAsync();
+    return Results.Text("You're not logged out.");
+});
+
+
+
 app.MapReverseProxy();
 app.Run();
 
